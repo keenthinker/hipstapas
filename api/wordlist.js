c4062e78777f0ad61c9418e714394df5da240ae9
keenthinker <pastxyz@gmail.com>
2020-06-23 23:24:23 +0200

    minor refactoring of the wordlist api

--- api/wordlist.js
+++ api/wordlist.js
@@ -3,14 +3,21 @@ import { wordlist1Map } from '../modules/wordlist1.js';
 
 module.exports = (req, res) => {
     var words = [];
-    for (var i = 0; i < 5; i++) {
-        var pos1 = randomNumber(1, 6);
-        var pos2 = randomNumber(1, 6);
-        var pos3 = randomNumber(1, 6);
-        var pos4 = randomNumber(1, 6);
-        // TODO just to be sure there are no duplicates - check every new pos that it does not match previous ones! 
-        let pos = `${pos1}${pos2}${pos3}${pos4}`;
-        words.push(wordlist1Map.get(pos));
+    var rolls = new Map();
+    var i = 0;
+    while (i < 5) {
+        var roll1 = randomNumber(1, 6);
+        var roll2 = randomNumber(1, 6);
+        var roll3 = randomNumber(1, 6);
+        var roll4 = randomNumber(1, 6);
+        // Just to be sure there are no duplicates,
+        // check that every new roll does not match previous ones! 
+        let roll = `${roll1}${roll2}${roll3}${roll4}`;
+        if (!rolls.has(roll)) {
+            rolls.set(roll, roll);
+            words.push(wordlist1Map.get(roll));
+            i++;
+        }
     }
     res.status(200).send(words.join(' '));
 }
