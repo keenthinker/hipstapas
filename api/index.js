49bf0c47654f10377c9ce9d51dccde357a5658dd
keenthinker <pastxyz@gmail.com>
2020-07-10 22:52:39 +0200

    refactoring, new options and query parameters

--- api/index.js
+++ api/index.js
@@ -41,22 +41,9 @@ function calculateLowerAndUpperBound(lmin, lmax) {
   return { "lower": lowerBound, "upper": upperBound };
 }
 
-/**
- * Generates a random string considering the specified options
- * @param {Object} options 
- */
-function generate(options) {
-  let lmin = options.lengthMin;
-  let lmax = options.lengthMax;
-
-  let alphabetSmall = "abcdefghijklmnopqrstuvwxyz";
-  let alphabetCapital = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  let alphabetNumbers = "0123456789";
-  let alphabetSpecial = ".,+-*/!?;:{}()[]%$&~#@|";
-  let alphabet = alphabetSmall + alphabetCapital + alphabetNumbers + alphabetSpecial;
-
+function generatePhrase(alphabet, lengthMin, lengthMax) {
   let text = [];
-  var bounds = calculateLowerAndUpperBound(lmin, lmax);
+  var bounds = calculateLowerAndUpperBound(lengthMin, lengthMax);
   let lowerBound = bounds.lower;
   for (let i = 0; i < lowerBound; i++) {
     text.push(randomCharacter(alphabet));
@@ -68,19 +55,131 @@ function generate(options) {
   return text.join('');
 }
 
+/**
+ * Generates a random string considering the specified options
+ * @param {Object} options 
+ */
+function generate(options) {
+  let lengthMin = options.lengthMin;
+  let lengthMax = options.lengthMax;
+  let resultsCount = options.resultsCount;
+
+  let alphabetSmall = "abcdefghijklmnopqrstuvwxyz";
+  let alphabetCapital = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let alphabetNumbers = "0123456789";
+  let alphabetSpecial = ".,+-*/!?;:{}()[]%$&~#@|";
+  let alphabet = alphabetSmall + alphabetCapital + alphabetNumbers + alphabetSpecial;
+
+  let passPhrases = [];
+  for (let count = 0; count < resultsCount; count++) {
+    passPhrases.push(generatePhrase(alphabet, lengthMin, lengthMax));
+  }
+
+  return passPhrases;
+}
+
+function validate(v, vr, cb) {
+  // check if parameter is set
+  if (v) {
+    let validationOk = false;
+    let errorMessage = "";
+    let value = Number(v);
+    for (let i = 0; i < vr.rules.length; i++) {
+      let rm = vr.rules[i];
+      validationOk = rm.check(value);
+      if (!validationOk) {
+        errorMessage = rm.message;
+        break;
+      } 
+    }
+    cb(value);
+    return {
+      "success": validationOk,
+      "error": errorMessage
+    };
+  }
+  // if parameter is not set, then pass
+  return {
+    "success": true,
+    "error": ""
+  };
+}
+
 module.exports = (req, res) => {
-    var lmin = 16;
-    var lmax = 32;
-    if (req.body) {
-      lmin = req.body.min || 16;
-      lmax = req.body.max || 32;
+    let resultsCount = 1;
+    let lengthMin = 16;
+    let lengthMax = 32;
+    // if (req.body) {
+    //   lengthMin = req.body.lengthMin || 16;
+    //   lengthMax = req.body.lengthMax || 32;
+    //   resultsCount = req.body.resultsCount || 1;
+    // }
+    // query validation - todo: use npm jsonschema
+    if (req.query) {
+      if (req.query !== undefined && JSON.stringify(req.query) !== '{}') {
+        //console.log(req.query);
+        var validateLengthMin = validate(req.query.lengthMin, {
+          rules: [
+            {
+              "check": function (v) { return Number.isInteger(v); },
+              "message": "Only numbers between 1 and 2048 are allowed as values for the query parameter \"lengthMin\". Example: https://hipstapas.dev/api/?lengthMin=10"  
+            },
+            {
+              "check": function (v) { return v >= 1 && v <= 2048; },
+              "message": "Query parameter value must be between 1 and 2048. Example: https://hipstapas.dev/api/?lengthMin=10"
+            }
+          ]
+        }, (r) => lengthMin = r);
+
+        var validateLengthMax = validate(req.query.lengthMax, {
+          rules: [
+            {
+              "check": function (v) { return Number.isInteger(v); },
+              "message": "Only numbers between 1 and 2048 are allowed as values for the query parameter \"lengthMax\". Example: https://hipstapas.dev/api/?lengthMax=10"  
+            },
+            {
+              "check": function (v) { return v >= 1 && v <= 2048; },
+              "message": "Query parameter value must be between 1 and 2048. Example: https://hipstapas.dev/api/?lengthMax=10"
+            }
+          ]
+        }, (r) => lengthMax = r);
+
+        var validateResultsCount = validate(req.query.resultsCount, {
+          rules: [
+            {
+              "check": function (v) { return Number.isInteger(v); },
+              "message": "Only numbers between 1 and 100 are allowed as values for the query parameter \"resultsCount\". Example: https://hipstapas.dev/api/?resultsCount=10"  
+            },
+            {
+              "check": function (v) { return v >= 1 && v <= 100; },
+              "message": "Query parameter value must be between 1 and 100. Example: https://hipstapas.dev/api/?resultsCount=10"
+            }
+          ]
+        }, (r) => resultsCount = r);
+
+        if (!validateLengthMin.success) {
+          res.status(200).send(validateLengthMin.error);
+          return;
+        }
+
+        if (!validateLengthMax.success) {
+          res.status(200).send(validateLengthMax.error);
+          return;
+        }
+
+        if (!validateResultsCount.success) {
+          res.status(200).send(validateResultsCount.error);
+          return;
+        }
+      }
     }
     //res.setHeader('Content-Type', 'application/json');
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-Id, Content-Type, Accept');
-    res.status(200).send(generate({ 
-      lengthMin: lmin, 
-      lengthMax: lmax,
-      resultsCount: 1
-     }));
+    let results = generate({ 
+      lengthMin: lengthMin, 
+      lengthMax: lengthMax,
+      resultsCount: resultsCount
+     });
+    res.status(200).send(results.length == 1 ? results[0] : results);
   }
