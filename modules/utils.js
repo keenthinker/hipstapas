aa3a9c2b72927690ba6d10f4900de3f6a35da086
keenthinker <1250439+keenthinker@users.noreply.github.com>
2021-01-24 22:53:01 +0100

    removed logging code; optimized content

--- modules/utils.js
+++ modules/utils.js
@@ -1,17 +1,3 @@
-function logEndpointCall(endpointName) {
-        const https = require('https');
-        const hostname = 'hipstalog.vercel.app';
-        const port = 443;
-        https.request({
-                hostname: hostname,
-                port: port,
-                path: `/api/index?counterType=${endpointName}`,
-                method: 'GET',
-                headers: { 'x-logger-secret': process.env.HIPSTALOG_CALLER_KEY }
-                })
-                .end();
-}
-
 function generationResult(success, results, error) {
         return {
                 "success": success,
@@ -20,4 +6,4 @@ function generationResult(success, results, error) {
         };
 }
 
-module.exports = { logEndpointCall, generationResult };
+module.exports = { generationResult };
